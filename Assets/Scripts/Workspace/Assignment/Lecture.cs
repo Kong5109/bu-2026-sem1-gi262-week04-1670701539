7d94bf180966e8378c8b41f3d243d0821be99dcd
Kong5109 <kong8071.kk@gmail.com>
2026-09-08 14:09:12 +0700

    Workshop

--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -8,9 +8,9 @@ namespace Assignment
         public void Start()
         {
             // LCT01_SyntaxList();
-            // LCT02_SyntaxLinkedList();
+            //LCT02_SyntaxLinkedList();
             // LCT03_SyntaxHashTable();
-            // LCT04_SyntaxDictionary();
+            LCT04_SyntaxDictionary();
         }
 
         #region Lecture
@@ -22,7 +22,35 @@ namespace Assignment
 
         public void LCT02_SyntaxLinkedList()
         {
-            throw new System.NotImplementedException();
+            LinkedList<string> linkedList = new LinkedList<string>();
+            linkedList.AddLast("Node 1");
+            linkedList.AddLast("Node 2");
+            linkedList.AddFirst("Node 0");
+
+            LinkedListNode<string> node1 = linkedList.Find("Node 1");
+            Debug.Log(node1.Value);
+            Debug.Log(node1.Next.Value);
+            Debug.Log(node1.Previous.Value);
+            Debug.Log("\n=========( 1 )=========\n");
+
+            var firstNode = linkedList.First;
+            var lastNode = linkedList.Last;
+            Debug.Log(firstNode.Next);
+            Debug.Log(lastNode.Next);
+            Debug.Log("\n=========( 2 )=========\n");
+
+            linkedList.AddAfter(node1, "Node 1.5");
+            linkedList.AddBefore(node1, "Node 0.5");
+
+            linkedList.RemoveFirst();
+            linkedList.RemoveLast();
+            linkedList.Remove("Node 1.5"); // Remove First one
+            linkedList.Clear();
+            foreach (var item in linkedList)
+            {
+                Debug.Log(item);
+            }
+            Debug.Log("\n=========( 3 )=========\n");
         }
 
         public void LCT03_SyntaxHashTable()
@@ -32,7 +60,28 @@ namespace Assignment
 
         public void LCT04_SyntaxDictionary()
         {
-            throw new System.NotImplementedException();
+            Dictionary<string, int> inv = new Dictionary<string, int>();
+            inv.Add("Potion", 5);
+            inv.Add("Banana", 1);
+            inv.Add("Apple", 10);
+
+            inv["Apple"] = 0;
+            inv["Apple1"] = 1; // if no key => Create new key
+
+            int potion = inv["Potion"];
+            Debug.Log($"Potion " + potion);
+
+            bool hasPotion = inv.ContainsKey("Potion");
+            Debug.Log($"hasPotion" + hasPotion);
+
+            inv.Remove("Banana");
+            Debug.Log("\n=========( 1 )=========\n");
+            foreach (KeyValuePair<string, int> kvp in inv)
+            {
+                var key = kvp.Key;
+                var value = kvp.Value;
+                Debug.Log($"Key: {key} Has Value {value}");
+            }
         }
 
         #endregion
--- Assets/Workshop/Student/Scripts/ZombieParade.cs
+++ Assets/Workshop/Student/Scripts/ZombieParade.cs
@@ -53,22 +53,54 @@ namespace Solution
         IEnumerator MoveParade()
         {
             //0. สร้างหัวงู
+            Parade.AddFirst(this.gameObject);
 
             while (isAlive)
             {
                 // 1. ดึงส่วนแรกของงูออกมา
+                var firstNode = Parade.First;
+                var firstGO = firstNode.Value;
 
                 // 2. ดึงส่วนสุดท้ายของงูออกมา
-             
+                var lastNode = Parade.Last;
+                var lastGO = lastNode.Value;
+
+
                 // 3. ลบส่วนสุดท้ายออกจาก LinkedList
+                Parade.RemoveLast();
 
                 // 5. กำหนดตำแหน่งและทิศทางของส่วนที่ถูกย้ายมาใหม่
                 // ให้ไปอยู่ที่ตำแหน่งของส่วนหัวงู (ซึ่งเพิ่งเคลื่อนที่ไปเมื่อครู่)
-   
+                int toX = 0;
+                int toY = 0;
+
+                moveDirection = RandomizeDirection();
+                toX = (int)(firstGO.transform.position.x + moveDirection.x);
+                toY = (int)(firstGO.transform.position.y + moveDirection.y);
+                while (IsCollision(toX, toY))
+                {
+                    moveDirection = RandomizeDirection();
+                    toX = (int)(firstGO.transform.position.x + moveDirection.x);
+                    toY = (int)(firstGO.transform.position.y + moveDirection.y);
+                    yield return null;
+                }
+
                 //6. เคลื่อนที่
+                positionX = toX;
+                positionY = toY;
+                lastGO.transform.position = new Vector3(positionX, positionY, 0);
+                if (moveDirection == Vector3.right)
+                {
+                    lastGO.GetComponent<SpriteRenderer>().flipX = true;
+                }
+                else if (moveDirection == Vector3.left)
+                {
+                    lastGO.GetComponent<SpriteRenderer>().flipX = false;
+                }
 
                 // 7. เพิ่มส่วนนั้นกลับเข้าไปเป็นส่วนที่สองของ LinkedList
                 // (ซึ่งก็คือส่วนแรกของลำตัว)
+                Parade.AddFirst(lastGO);
 
                 // รอตามเวลาที่กำหนดก่อนการเคลื่อนที่ครั้งต่อไป
                 yield return new WaitForSeconds(moveInterval);
@@ -77,7 +109,11 @@ namespace Solution
         private bool IsCollision(int x, int y)
         {
             // 4. ตรวจสอบสิ่งกีดขวาง
-            
+            if (HasPlacement(x, y))
+            {
+                return true;
+            }
+
             return false;
         }
         void Move(Vector2 direction,GameObject targetMove)
